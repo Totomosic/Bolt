4a9f5d8c10ca2839528b630559fb803569ce62ff
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2019-01-22 21:00:08 +0800

    Updated EC2 location

--- DND/src/App.h
+++ DND/src/App.h
@@ -12,7 +12,7 @@ namespace DND
 		blt::string ADDRESS = "192.168.1.77";
 		blt::string TARGET_ADDRESS = "localhost";
 
-		blt::string EC2_ADDRESS = "ec2-13-58-43-219.us-east-2.compute.amazonaws.com";
+		blt::string EC2_ADDRESS = "ec2-18-219-148-3.us-east-2.compute.amazonaws.com";
 		int EC2_PORT = 12345;
 
 	public:
