57a6769006381209771912d7a2f0093990087743
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2019-08-20 21:16:49 +0800

    Added a scoped listener manager to UImanager. Added EventEmitterBase as base class for GenericEventEmitter

--- Bolt-Engine/Bolt-Core/src/Core/Events/EventEmitter.h
+++ Bolt-Engine/Bolt-Core/src/Core/Events/EventEmitter.h
@@ -6,16 +6,25 @@ namespace Bolt
 
 	template<typename> class GenericEventBus;
 
-	template<typename T, typename EventIdT>
-	class BLT_API GenericEventEmitter
+	class BLT_API EventEmitterBase
 	{
+	public:
+		virtual void RemoveEventListener(uint32_t listenerId) = 0;
+	};
+
+	template<typename T, typename EventIdT>
+	class BLT_API GenericEventEmitter : public EventEmitterBase
+	{
+	public:
+		using callback_t = typename EventListener<T>::callback_t;
+
 	private:
 		EventIdT m_EventId;
-		GenericEventBus<EventIdT>& m_EventBus;
+		GenericEventBus<EventIdT>* m_EventBus;
 
 	public:
 		GenericEventEmitter(const EventIdT& eventId, GenericEventBus<EventIdT>& bus)
-			: m_EventId(eventId), m_EventBus(bus)
+			: m_EventId(eventId), m_EventBus(&bus)
 		{
 		
 		}
@@ -27,32 +36,32 @@ namespace Bolt
 
 		GenericEventBus<EventIdT>& Bus()
 		{
-			return m_EventBus;
+			return *m_EventBus;
 		}
 
-		uint32_t AddEventListener(const typename EventListener<T>::callback_t& callback, ListenerPriority priority = ListenerPriority::Medium)
+		uint32_t AddEventListener(const typename callback_t& callback, ListenerPriority priority = ListenerPriority::Medium)
 		{
-			return m_EventBus.AddEventListener<T>(m_EventId, callback, priority);
+			return m_EventBus->AddEventListener<T>(m_EventId, callback, priority);
 		}
 
-		void RemoveEventListener(uint32_t listenerId)
+		void RemoveEventListener(uint32_t listenerId) override
 		{
-			m_EventBus.RemoveEventListener(listenerId);
+			m_EventBus->RemoveEventListener(listenerId);
 		}
 
 		void Emit(const T& data)
 		{
-			m_EventBus.Emit(m_EventId, data);
+			m_EventBus->Emit(m_EventId, data);
 		}
 
 		void Emit(T&& data)
 		{
-			m_EventBus.Emit(m_EventId, std::move(data));
+			m_EventBus->Emit(m_EventId, std::move(data));
 		}
 
 		void Emit()
 		{
-			m_EventBus.Emit(m_EventId);
+			m_EventBus->Emit(m_EventId);
 		}
 
 	};
