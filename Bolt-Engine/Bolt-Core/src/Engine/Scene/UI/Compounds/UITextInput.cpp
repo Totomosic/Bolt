d09270342f55d638f97e739e96e1f6118d7e900a
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2019-08-23 21:02:05 +0800

    Added obfuscation to UITextInput

--- Bolt-Engine/Bolt-Core/src/Engine/Scene/UI/Compounds/UITextInput.cpp
+++ Bolt-Engine/Bolt-Core/src/Engine/Scene/UI/Compounds/UITextInput.cpp
@@ -6,7 +6,7 @@ namespace Bolt
 {
 
 	UITextInput::UITextInput(UIManager* manager, UIElement* parent, float width, float height, const ResourcePtr<Font>& font, const Color& fontColor, std::unique_ptr<Material>&& backgroundMaterial, Transform&& transform) : UICompoundElement(manager, parent),
-		m_Background(nullptr), m_Text(nullptr)
+		m_Background(nullptr), m_Text(nullptr), m_String(""), m_Obfuscate(false)
 	{
 		SetGameObject(m_Manager->Factory().Instantiate(std::move(transform)));
 		m_Background = &CreateSurface(width, height, std::move(backgroundMaterial));
@@ -26,24 +26,55 @@ namespace Bolt
 
 	const blt::string& UITextInput::GetText() const
 	{
-		return m_Text->GetText();
+		return m_String;
+	}
+
+	bool UITextInput::IsObfuscating() const
+	{
+		return m_Obfuscate;
+	}
+
+	void UITextInput::SetObfuscating(bool enabled)
+	{
+		bool different = m_Obfuscate != enabled;
+		m_Obfuscate = enabled;
+		if (different)
+		{
+			TextElement().SetText(GetTextRepresentation());
+		}
 	}
 
 	void UITextInput::SetupEventHandlers()
 	{
 		Events().OnCharPressed.AddEventListener([this](Event<UICharPressedEvent>& e)
 			{
-				TextElement().SetText(TextElement().GetText() + (char)e.Data.CharCode);
+				m_String += (char)e.Data.CharCode;
+				TextElement().SetText(GetTextRepresentation());
 				e.StopPropagation();
 			});
 		Events().OnKeyDown.AddEventListener([this](Event<UIKeyDownEvent>& e)
 			{
 				if (e.Data.Key == Keycode::Backspace && GetText().size() > 0)
 				{
-					TextElement().SetText(GetText().substr(0, GetText().length() - 1));
+					m_String.pop_back();
+					TextElement().SetText(GetTextRepresentation());
 				}
 				e.StopPropagation();
 			});
+	}
+
+	blt::string UITextInput::GetTextRepresentation() const
+	{
+		if (m_Obfuscate)
+		{
+			blt::string result = "";
+			for (int i = 0; i < m_String.size(); i++)
+			{
+				result += '*';
+			}
+			return result;
+		}
+		return m_String;
 	}
 
 }
--- Bolt-Engine/Bolt-Core/src/Engine/Scene/UI/Compounds/UITextInput.h
+++ Bolt-Engine/Bolt-Core/src/Engine/Scene/UI/Compounds/UITextInput.h
@@ -11,6 +11,8 @@ namespace Bolt
 	private:
 		UISurface* m_Background;
 		UIText* m_Text;
+		blt::string m_String;
+		bool m_Obfuscate;
 
 	public:
 		UITextInput(UIManager* manager, UIElement* parent, float width, float height, const ResourcePtr<Font>& font, const Color& fontColor, std::unique_ptr<Material>&& backgroundMaterial, Transform&& transform);
@@ -19,9 +21,12 @@ namespace Bolt
 		UIText& TextElement() const;
 
 		const blt::string& GetText() const;
+		bool IsObfuscating() const;
+		void SetObfuscating(bool enabled);
 
 	private:
 		void SetupEventHandlers();
+		blt::string GetTextRepresentation() const;
 
 	};
 
