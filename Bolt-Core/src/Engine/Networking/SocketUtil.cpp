a09717a87d7eaf55fa4af576159580665dc82a7f
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2020-02-18 20:09:10 +0800

    Modified select

--- Bolt-Core/src/Engine/Networking/SocketUtil.cpp
+++ Bolt-Core/src/Engine/Networking/SocketUtil.cpp
@@ -46,146 +46,22 @@ namespace Bolt
 		return result;
 	}
 
-	int SocketUtil::Select(const UDPsocketSet* inReadSet, UDPsocketSet* outReadSet,
-		const UDPsocketSet* inWriteSet, UDPsocketSet* outWriteSet,
-		const UDPsocketSet* inErrorSet, UDPsocketSet* outErrorSet, int timeoutMilliseconds)
+	UDPsocket* SocketUtil::SelectRead(UDPsocket* socket, int timeoutMilliseconds)
 	{
-		fd_set read;
-		fd_set write;
-		fd_set error;
-
-		fd_set* readPtr = (inReadSet != nullptr) ? FillSetFromVector(read, *inReadSet) : nullptr;
-		fd_set* writePtr = (inWriteSet != nullptr) ? FillSetFromVector(write, *inWriteSet) : nullptr;
-		fd_set* errorPtr = (inErrorSet != nullptr) ? FillSetFromVector(error, *inErrorSet) : nullptr;
-
-		timeval timeout;
-		timeval* timeoutPtr = nullptr;
-		if (timeoutMilliseconds >= 0)
+		fd_set readSet;
+		FD_ZERO(&readSet);
+		FD_SET(socket->m_Socket, &readSet);
+		timeval t;
+		t.tv_sec = timeoutMilliseconds / 1000;
+		t.tv_usec = timeoutMilliseconds * 1000 - t.tv_sec * 1000000;
+		if (select(1, &readSet, nullptr, nullptr, &t) > 0)
 		{
-			timeout.tv_sec = timeoutMilliseconds / 1000;
-			timeout.tv_usec = (timeoutMilliseconds % 1000) * 1000;
-		}
-
-		int result = select(0, readPtr, writePtr, errorPtr, timeoutPtr);
-		if (result > 0)
-		{
-			FillVectorFromSet(*outReadSet, *inReadSet, read);
-			FillVectorFromSet(*outWriteSet, *inWriteSet, write);
-			FillVectorFromSet(*outErrorSet, *inErrorSet, error);
-		}
-		return result;
-	}
-
-	int SocketUtil::Select(const UDPsocketSet* inReadSet, UDPsocketSet* outReadSet,
-		const UDPsocketSet* inWriteSet, UDPsocketSet* outWriteSet, int timeoutMilliseconds)
-	{
-		return Select(inReadSet, outReadSet, inWriteSet, outWriteSet, nullptr, nullptr, timeoutMilliseconds);
-	}
-
-	int SocketUtil::Select(const UDPsocketSet* inReadSet, UDPsocketSet* outReadSet, int timeoutMilliseconds)
-	{
-		return Select(inReadSet, outReadSet, nullptr, nullptr, timeoutMilliseconds);
-	}
-
-	int SocketUtil::Select(const TCPsocketSet* inReadSet, TCPsocketSet* outReadSet,
-		const TCPsocketSet* inWriteSet, TCPsocketSet* outWriteSet,
-		const TCPsocketSet* inErrorSet, TCPsocketSet* outErrorSet, int timeoutMilliseconds)
-	{
-		fd_set read;
-		fd_set write;
-		fd_set error;
-
-		fd_set* readPtr = (inReadSet != nullptr) ? FillSetFromVector(read, *inReadSet) : nullptr;
-		fd_set* writePtr = (inWriteSet != nullptr) ? FillSetFromVector(write, *inWriteSet) : nullptr;
-		fd_set* errorPtr = (inErrorSet != nullptr) ? FillSetFromVector(error, *inErrorSet) : nullptr;
-
-		timeval timeout;
-		timeval* timeoutPtr = nullptr;
-		if (timeoutMilliseconds >= 0)
-		{
-			timeout.tv_sec = timeoutMilliseconds / 1000;
-			timeout.tv_usec = (timeoutMilliseconds % 1000) * 1000;
-		}
-
-		int result = select(0, readPtr, writePtr, errorPtr, timeoutPtr);
-		if (result > 0)
-		{
-			FillVectorFromSet(*outReadSet, *inReadSet, read);
-			FillVectorFromSet(*outWriteSet, *inWriteSet, write);
-			FillVectorFromSet(*outErrorSet, *inErrorSet, error);
-		}
-		return result;
-	}
-
-	int SocketUtil::Select(const TCPsocketSet* inReadSet, TCPsocketSet* outReadSet,
-		const TCPsocketSet* inWriteSet, TCPsocketSet* outWriteSet, int timeoutMilliseconds)
-	{
-		return Select(inReadSet, outReadSet, inWriteSet, outWriteSet, nullptr, nullptr, timeoutMilliseconds);
-	}
-
-	int SocketUtil::Select(const TCPsocketSet* inReadSet, TCPsocketSet* outReadSet, int timeoutMilliseconds)
-	{
-		return Select(inReadSet, outReadSet, nullptr, nullptr, timeoutMilliseconds);
-	}
-
-	fd_set* SocketUtil::FillSetFromVector(fd_set& set, const UDPsocketSet& sockets)
-	{
-		if (&sockets == nullptr)
-		{
-			return &set;
-		}
-		FD_ZERO(&set);
-		for (UDPsocket* socket : sockets)
-		{
-			FD_SET(socket->m_Socket, &set);
-		}
-		return &set;
-	}
-
-	void SocketUtil::FillVectorFromSet(UDPsocketSet& outSockets, const UDPsocketSet& inSockets, const fd_set& set)
-	{
-		if (&outSockets == nullptr || &inSockets == nullptr)
-		{
-			return;
-		}
-		outSockets.clear();
-		for (UDPsocket* socket : inSockets)
-		{
-			if (FD_ISSET(socket->m_Socket, &set))
+			if (FD_ISSET(socket->m_Socket, &readSet))
 			{
-				outSockets.push_back(socket);
+				return socket;
 			}
 		}
-	}
-
-	fd_set* SocketUtil::FillSetFromVector(fd_set& set, const TCPsocketSet& sockets)
-	{
-		if (&sockets == nullptr)
-		{
-			return &set;
-		}
-		FD_ZERO(&set);
-		for (TCPsocket* socket : sockets)
-		{
-			FD_SET(socket->m_Socket, &set);
-		}
-		return &set;
-	}
-
-	void SocketUtil::FillVectorFromSet(TCPsocketSet& outSockets, const TCPsocketSet& inSockets, const fd_set& set)
-	{
-		if (&outSockets == nullptr || &inSockets == nullptr)
-		{
-			return;
-		}
-		outSockets.clear();
-		for (TCPsocket* socket : inSockets)
-		{
-			if (FD_ISSET(socket->m_Socket, &set))
-			{
-				outSockets.push_back(socket);
-			}
-		}
+		return nullptr;
 	}
 
 }
--- Bolt-Core/src/Engine/Networking/SocketUtil.h
+++ Bolt-Core/src/Engine/Networking/SocketUtil.h
@@ -19,26 +19,7 @@ namespace Bolt
 
 		static std::vector<uint32_t> GetIP4Addresses();
 
-		static int Select(const UDPsocketSet* inReadSet, UDPsocketSet* outReadSet,
-			const UDPsocketSet* inWriteSet, UDPsocketSet* outWriteSet,
-			const UDPsocketSet* inErrorSet, UDPsocketSet* outErrorSet, int timeOutMilliseconds = -1);
-		static int Select(const UDPsocketSet* inReadSet, UDPsocketSet* outReadSet,
-			const UDPsocketSet* inWriteSet, UDPsocketSet* outWriteSet, int timeOutMilliseconds = -1);
-		static int Select(const UDPsocketSet* inReadSet, UDPsocketSet* outReadSet, int timeOutMilliseconds = -1);
-
-		static int Select(const TCPsocketSet* inReadSet, TCPsocketSet* outReadSet,
-			const TCPsocketSet* inWriteSet, TCPsocketSet* outWriteSet,
-			const TCPsocketSet* inErrorSet, TCPsocketSet* outErrorSet, int timeOutMilliseconds = -1);
-		static int Select(const TCPsocketSet* inReadSet, TCPsocketSet* outReadSet,
-			const TCPsocketSet* inWriteSet, TCPsocketSet* outWriteSet, int timeOutMilliseconds = -1);
-		static int Select(const TCPsocketSet* inReadSet, TCPsocketSet* outReadSet, int timeOutMilliseconds = -1);
-
-	private:
-		static fd_set* FillSetFromVector(fd_set& set, const UDPsocketSet& sockets);
-		static void FillVectorFromSet(UDPsocketSet& outSockets, const UDPsocketSet& inSockets, const fd_set& set);
-
-		static fd_set* FillSetFromVector(fd_set& set, const TCPsocketSet& sockets);
-		static void FillVectorFromSet(TCPsocketSet& outSockets, const TCPsocketSet& inSockets, const fd_set& set);
+		static UDPsocket* SelectRead(UDPsocket* socket, int timeoutMilliseconds = 0);
 
 	};
 
